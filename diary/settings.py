10c026d9532d444205bbf513a2e886b70707e475
John Smith <aisuruyamano@gamail.com>
2021-11-12 01:41:58 +0900

    Added files and changes required for deployment to heroku

--- diary/settings.py
+++ diary/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-mtzlqye85xyh#2^t1lfh83%rp)(dbj%lrejnnhk)i(wn9rh_3(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1','sleepy-stream-26145.herokuapp.com']
+ALLOWED_HOSTS = []
 
 #hamuga12hu8yh89123y8eycfhu438yf48fy
 
